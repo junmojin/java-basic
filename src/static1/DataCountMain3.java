f1742ef9c6d28c6d0bd7c72165c9f2b9fde0b749
junmojin <o3o060@naver.com>
2025-03-01 00:56:17 +0900

    static 변수2~3

---
+++ src/static1/DataCountMain3.java
@@ -0,0 +1,22 @@
+package static1;
+
+public class DataCountMain3 {
+    public static void main(String[] args) {
+        Data3 data1 = new Data3("A");
+        System.out.println("A count=" + Data3.count); //Data3이 클래스인데 클래스에 직접 접근하는것처럼 .(dot)사용함
+
+        Data3 data2 = new Data3("B");
+        System.out.println("B count=" + Data3.count);
+
+        Data3 data3 = new Data3("C");
+        System.out.println("C count=" + Data3.count);
+
+        //추가
+        //인스턴스를 통한 접근
+        Data3 data4 = new Data3("D");
+        System.out.println(data4.count);
+
+        //클래스를 통한 접근
+        System.out.println(Data3.count);
+    }
+}
